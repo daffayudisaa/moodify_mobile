225832a2c0eaae988ed3f5108bb891caefcfd38b
daffayudisaa <daffayudisa09@gmail.com>
2024-11-13 18:40:17 +0700

    feat: reset password page layout

--- lib/pages/auth/reset_password.dart
+++ lib/pages/auth/reset_password.dart
@@ -1,16 +1,226 @@
 import 'package:flutter/material.dart';
+import 'package:hugeicons/hugeicons.dart';
 
-class ResetPassword extends StatelessWidget {
-  const ResetPassword({super.key});
+class ResetPasswordPage extends StatelessWidget {
+  const ResetPasswordPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Reset Password"),
-      ),
-      body: const Center(
-        child: Text("Reset Password Page"),
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(), // Dismisses the keyboard
+        child: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color(0xFFA0D3F5),
+                Color(0xFFD9E9F8),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: Column(
+            children: [
+              AppBar(
+                toolbarHeight: 60,
+                backgroundColor: Colors.transparent,
+                elevation: 0,
+                leading: IconButton(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  icon: const HugeIcon(
+                    icon: HugeIcons.strokeRoundedArrowLeft02,
+                    color: Colors.black,
+                    size: 30,
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ),
+              Expanded(
+                child: Center(
+                  child: Column(
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 25),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Image(
+                                  image: AssetImage(
+                                      'assets/meowdy/Meowdy-Drink.png'),
+                                ),
+                              ],
+                            ),
+                            SizedBox(width: 20),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'New Password',
+                                    style: TextStyle(
+                                      color: Color(0xFF263238),
+                                      fontFamily: 'Poppins',
+                                      fontSize: 36,
+                                      fontWeight: FontWeight.w600,
+                                      height: 1,
+                                    ),
+                                  ),
+                                  SizedBox(height: 15),
+                                  Text(
+                                    'Set a new password and get back to enjoying Moodify.',
+                                    style: TextStyle(
+                                      fontFamily: 'Poppins',
+                                      fontSize: 13,
+                                      height: 1.2,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                            color: Colors.white,
+                            child: Padding(
+                              padding: const EdgeInsets.only(
+                                right: 40,
+                                left: 40,
+                                top: 30,
+                              ),
+                              child: SingleChildScrollView(
+                                child: Column(
+                                  children: [
+                                    const Text(
+                                      'Time to set a new password! Make it strong, unique, and something you’ll remember!',
+                                      style: TextStyle(
+                                        fontFamily: 'Poppins',
+                                        fontSize: 16,
+                                        height: 1.2,
+                                      ),
+                                      textAlign: TextAlign.center,
+                                    ),
+                                    const SizedBox(height: 30),
+                                    TextField(
+                                      decoration: InputDecoration(
+                                        hintText: 'New Password',
+                                        hintStyle: const TextStyle(
+                                          fontFamily: 'Poppins',
+                                          fontSize: 14,
+                                          color: Colors.black54,
+                                        ),
+                                        contentPadding:
+                                            const EdgeInsets.symmetric(
+                                          vertical: 15,
+                                          horizontal: 20,
+                                        ),
+                                        filled: true,
+                                        fillColor: const Color(0xFFA0D3F5)
+                                            .withOpacity(0.2),
+                                        border: InputBorder.none,
+                                        focusedBorder: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(6),
+                                          borderSide: const BorderSide(
+                                            color: Color(0xFF008EF2),
+                                            width: 2,
+                                          ),
+                                        ),
+                                        enabledBorder: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(6),
+                                          borderSide: const BorderSide(
+                                            color: Colors.transparent,
+                                            width: 1,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    const SizedBox(height: 20),
+                                    TextField(
+                                      decoration: InputDecoration(
+                                        hintText: 'Confirm New Password',
+                                        hintStyle: const TextStyle(
+                                          fontFamily: 'Poppins',
+                                          fontSize: 14,
+                                          color: Colors.black54,
+                                        ),
+                                        contentPadding:
+                                            const EdgeInsets.symmetric(
+                                          vertical: 15,
+                                          horizontal: 20,
+                                        ),
+                                        filled: true,
+                                        fillColor: const Color(0xFFA0D3F5)
+                                            .withOpacity(0.2),
+                                        border: InputBorder.none,
+                                        focusedBorder: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(6),
+                                          borderSide: const BorderSide(
+                                            color: Color(0xFF008EF2),
+                                            width: 2,
+                                          ),
+                                        ),
+                                        enabledBorder: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(6),
+                                          borderSide: const BorderSide(
+                                            color: Colors.transparent,
+                                            width: 1,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    const SizedBox(height: 40),
+                                    ElevatedButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                        Navigator.pushNamedAndRemoveUntil(
+                                            context,
+                                            '/signin',
+                                            (Route<dynamic> route) => false);
+                                      },
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor:
+                                            const Color(0xFF0096FF),
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(6),
+                                        ),
+                                        padding: const EdgeInsets.symmetric(
+                                            horizontal: 93, vertical: 15),
+                                      ),
+                                      child: const Text(
+                                        "Save Password",
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.w600,
+                                          fontFamily: 'Poppins',
+                                        ),
+                                      ),
+                                    ),
+                                    const SizedBox(height: 40),
+                                  ],
+                                ),
+                              ),
+                            )),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
