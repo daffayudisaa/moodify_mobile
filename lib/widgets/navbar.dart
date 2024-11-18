43c25c96f05af6902bc2c1bfec3aaaa48a5d9788
daffayudisaa <daffayudisa09@gmail.com>
2024-11-18 22:48:36 +0700

    style: update navbar icon

--- lib/widgets/navbar.dart
+++ lib/widgets/navbar.dart
@@ -5,7 +5,6 @@ import 'package:moodify_mobile/pages/music_recom_page.dart';
 import 'package:moodify_mobile/pages/preparescan_page.dart';
 import 'package:moodify_mobile/pages/profile.dart';
 import 'package:moodify_mobile/pages/recap_mood.dart';
-import 'package:hugeicons/hugeicons.dart';
 
 class Navbar extends StatefulWidget {
   final CameraDescription camera;
@@ -38,29 +37,27 @@ class _NavbarState extends State<Navbar> {
 
   double _getIconSize(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
-    if (screenWidth > 600) {
-      return 34.0;
-    } else if (screenWidth > 400) {
-      return 30.0;
+    if (screenWidth > 400) {
+      return 28.0;
+    } else {
+      return 25.0;
     }
-    return 26.0;
   }
 
   double _getTextSize(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     if (screenWidth > 600) {
-      return 15.0;
-    } else if (screenWidth > 400) {
-      return 13.0;
+      return 14.0;
+    } else {
+      return 11.0;
     }
-    return 11.0;
   }
 
   double _getBottomBarHeight(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
-    if (screenHeight > 800) {
-      return 90.0;
-    } else if (screenHeight > 600) {
+    if (screenHeight > 600) {
+      return 95.0;
+    } else if (screenHeight > 400) {
       return 80.0;
     }
     return 70.0;
@@ -82,17 +79,17 @@ class _NavbarState extends State<Navbar> {
       floatingActionButton: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.03),
           SizedBox(
             height: fabSize,
             width: fabSize,
             child: FloatingActionButton(
               backgroundColor: Colors.blue,
               splashColor: Colors.transparent,
-              child: Icon(
-                HugeIcons.strokeRoundedFaceId,
+              child: Image(
+                image: const AssetImage('assets/icons/navbar/Scan.png'),
                 color: Colors.white,
-                size: iconSize * 1.2,
+                width: iconSize * 1.4,
               ),
               onPressed: () {
                 Navigator.of(context).push(
@@ -121,10 +118,10 @@ class _NavbarState extends State<Navbar> {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-                    _buildNavItem(0, HugeIcons.strokeRoundedHome09, "Home",
-                        iconSize, textSize),
-                    _buildNavItem(1, HugeIcons.strokeRoundedDocumentValidation,
-                        "Recap", iconSize, textSize),
+                    _buildNavItem(0, 'assets/icons/navbar/Home.png', "Home",
+                        iconSize * 1.1, textSize),
+                    _buildNavItem(1, 'assets/icons/navbar/Recap-Mood.png',
+                        "Recap", iconSize * 1.15, textSize),
                   ],
                 ),
               ),
@@ -133,10 +130,10 @@ class _NavbarState extends State<Navbar> {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-                    _buildNavItem(2, HugeIcons.strokeRoundedFolderMusic,
-                        "Music", iconSize, textSize),
-                    _buildNavItem(3, HugeIcons.strokeRoundedUser, "Profile",
-                        iconSize, textSize),
+                    _buildNavItem(2, 'assets/icons/navbar/Music-Recom.png',
+                        "Music", iconSize * 1.1, textSize),
+                    _buildNavItem(3, 'assets/icons/navbar/Profile.png',
+                        "Profile", iconSize * 1.1, textSize),
                   ],
                 ),
               ),
@@ -147,8 +144,8 @@ class _NavbarState extends State<Navbar> {
     );
   }
 
-  Widget _buildNavItem(int index, IconData icon, String label, double iconSize,
-      double textSize) {
+  Widget _buildNavItem(int index, String imagePath, String label,
+      double iconSize, double textSize) {
     return MaterialButton(
       highlightColor: Colors.transparent,
       splashColor: Colors.transparent,
@@ -163,16 +160,17 @@ class _NavbarState extends State<Navbar> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Icon(
-            icon,
-            size: iconSize,
-            color: currentTab == index ? Colors.blue : Colors.grey,
+          Image.asset(
+            imagePath,
+            width: iconSize,
+            height: iconSize,
+            color: currentTab == index ? Colors.blue : Colors.grey.shade400,
           ),
           Text(
             label,
             style: TextStyle(
               fontSize: textSize,
-              color: currentTab == index ? Colors.blue : Colors.grey,
+              color: currentTab == index ? Colors.blue : Colors.grey.shade400,
             ),
           ),
         ],
