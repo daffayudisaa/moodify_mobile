b77143f2170ac53b4489816e58e825e041a1d452
rendyputrak <rendyputrak@gmail.com>
2024-12-02 20:32:16 +0700

    feat: sign in functionality

--- lib/presentation/bloc/auth/auth_bloc.dart
+++ lib/presentation/bloc/auth/auth_bloc.dart
@@ -1,6 +1,8 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'auth_event.dart';
-import 'auth_state.dart';
+import 'package:moodify_mobile/presentation/bloc/auth/auth_event.dart';
+import 'package:moodify_mobile/presentation/bloc/auth/auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   AuthBloc() : super(AuthInitial()) {
@@ -12,15 +14,32 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
       SignInRequested event, Emitter<AuthState> emit) async {
     emit(AuthLoading());
     try {
-      // Simulasi proses autentikasi
-      await Future.delayed(const Duration(seconds: 2));
+      // Panggil API untuk autentikasi
+      final response = await http.post(
+        Uri.parse(
+            'http://10.0.2.2:8000/login'), // Ganti dengan URL API yang sesuai
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: json.encode({
+          'email': event.email,
+          'password': event.password,
+        }),
+      );
 
-      if (event.email == "moodify" && event.password == "123") {
+      // Cek status code dari response
+      if (response.statusCode == 200) {
+        // Jika login sukses, anggap server mengembalikan token atau informasi lain
+        final data = json.decode(response.body);
+
+        // Lakukan tindakan lain jika perlu (misal, menyimpan token)
         emit(AuthAuthenticated());
       } else {
+        // Jika login gagal
         emit(const AuthError("Invalid email or password"));
       }
     } catch (e) {
+      // Tangani error apabila ada masalah saat request API
       emit(AuthError(e.toString()));
     }
   }
